22e5b433f4e0ed0a1c08e715366b349e2e1bf134
Julian Mazaira <mazairaj94@gmail.com>
2017-04-12 14:49:37 -0700

    pushing to cloud

--- index.ios.js
+++ index.ios.js
@@ -4,28 +4,91 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
 
+var t = require('tcomb-form-native');
+var Form = t.form.Form;
+
+var weight = t.refinement(t.Number, function (n) { return n > 0; });
+
+weight.getValidationErrorMessage = function (value, path, context) {
+  return 'capacity cannot be less than zero: ' + context.locale;
+};
+var Workout = t.struct({
+  name: t.String,
+  split1: t.String,
+  split2: t.String,
+  split3: t.String,
+  weight: weight,
+  timeStart: t.Date
+})
+
+var options = {
+  auto: 'placeholders',
+  fields: {
+    timeStart: {
+      mode: 'time'
+    }
+  }
+};
+
+
 export default class TeamManager extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: {
+        name: "",
+        split1: "",
+        split2: "",
+        split3: "",
+        weight: 150
+      }
+    }
+  }
+  onChange(value) {
+    this.setState({value});
+  }
+
+  onPress() {
+    console.log("Pressed")
+    var value = this.refs.form.getValue();
+    this.setState({value: {weight: 150}});
+    if(value) {
+      var copy = Object.assign({}, value);
+
+      fetch("http://localhost:8080/postworkout", {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            workoutData: copy
+          })
+        })
+      }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style = {{fontSize: 25, fontWeight: '700', color: '#323232', marginTop: 20}}>Post a Workout </Text>
+        <Form
+          ref="form"
+          type={Workout}
+          options = {options}
+          onChange = {this.onChange.bind(this)}
+          value = {this.state.value}
+        />
+        <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this)} underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -38,16 +101,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  buttonText: {
+  fontSize: 18,
+  color: 'white',
+  alignSelf: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+}
 });
 
 AppRegistry.registerComponent('TeamManager', () => TeamManager);
--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,17 +1,204 @@
 "use strict";
 var express = require('express');
 var router = express.Router();
+var _ = require('underscore')
 
 const Workout = require('../models/models').Workout;
 
+//GoogleSheets
+var GoogleSpreadsheet = require('google-spreadsheet');
+var doc = new GoogleSpreadsheet('1EQivJ6AIqFxCCEvJVqw-34W3llXdrE4Qi9k_mjJgjco');
+var sheet;
+var creds = require('../My_Project_Cred');
+var async = require('async')
+
+doc.useServiceAccountAuth(creds, function(err, item){
+  if (err) {
+    console.log(err)
+  } else {
+    console.log("Authenticated")
+  }
+})
 
 const bodyParser = require('body-parser')
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json())
 
-router.get('/test', function(req, res){
-  res.send('test')
-});
+var callback = function(res, string){
+  if (res) {
+    console.log(res)
+  }
+  console.log(string)
+}
+var setAuth = function() {
+  console.log("This is Doc: ", doc)
+  doc.useServiceAccountAuth(creds)
+  callback(null, "callback");
+}
+var workingWithCells = function() {
+  console.log("<<<<<<<< Inside working cells >>>>>>")
+  sheet.setHeaderRow(['name', 'split1', 'split2', 'split3', 'weight'], function(err, done){
+    if (err) {
+      console.log(err)
+    } else {
+      callback(null, "callback");
+    }
+  })
+}
+var getInfoAndWorksheets = function(){
+  doc.getInfo(function(err, info) {
+    console.log("<<<<<<<< Inside Get INfo >>>>>>")
+    console.log('Loaded doc: '+info.title+' by '+info.author.email);
+    sheet = info.worksheets[0];
+    console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("INFO", info);
+    }
+  });
+}
+var addRowData = function(){
+  console.log('ADD ROW')
+  sheet.addRow({
+    "name" : req.body.name,
+    "split1": req.body.split1,
+    "split2": req.body.split2,
+    "split3": req.body.split3,
+    "weight": req.body.weight
+  }, function(err){
+    if (err){
+      console.log(err)
+    } else {
+      callback(null, 'callback')
+    }
+  })
+}
+
+
+router.post('/postworkout', function(req, res){
+  var data = req.body.workoutData
+  console.log("This is REQd", req.body)
+  async.series([
+    function(callback) {
+      doc.getInfo(function(err, info) {
+        console.log("<<<<<<<< Inside Get INfo >>>>>>")
+        console.log('Loaded doc: '+info.title+' by '+info.author.email);
+        sheet = info.worksheets[0];
+        console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+        console.log("INFO", info)      // do some stuff ...
+        callback(null, 'one');
+      })
+    },
+    function(callback) {
+      console.log("<<<<<<<< Inside working cells >>>>>>")
+      sheet.setHeaderRow(['name', 'split1', 'split2', 'split3', 'weight'], function(err, done){
+        if (err) {
+          console.log(err)
+        } else {
+          callback(null, "callback");
+        }
+      })
+    },
+    function(callback){
+      console.log("In Get Cells")
+      sheet.getRows(function(err, rows){
+        if (err) {
+          console.log("Error: ", err)
+        } else {
+          var index;
+          rows.forEach(function(rowData, i) {
+            
+            if (rowData.name === data.name) {
+              console.log(rowData.name)
+              index = i
+            }
+
+            if (index) {
+              console.log("IN Index")
+              rows[index].del()
+            }
+          });
+        }
+        console.log("Rows", rows)
+        callback(null, 'callback')
+
+      })
+    },
+    function(callback) {
+      console.log('ADD ROW')
+      sheet.addRow({
+        "name" : data.name,
+        "split1": data.split1,
+        "split2": data.split2,
+        "split3": data.split3,
+        "weight": data.weight
+      }, function(err, rows){
+        if (err){
+          console.log(err)
+        } else {
+          console.log("added Row", rows)
+          callback(null, 'callback')
+        }
+      })
+    }
+  ])
+})
+// router.post('/test', function(req, res){
+//   async.series([
+//   function(callback) {
+//     doc.getInfo(function(err, info) {
+//       console.log("<<<<<<<< Inside Get INfo >>>>>>")
+//         console.log('Loaded doc: '+info.title+' by '+info.author.email);
+//         sheet = info.worksheets[0];
+//         console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);      // do some stuff ...
+//       callback(null, 'one');
+//   })
+// },
+//     function(callback)
+//         // do some more stuff ...
+//         console.log("<<<<<<<< Inside working cells >>>>>>")
+//         sheet.setHeaderRow(['name', 'split1', 'split2', 'split3', 'weight'], function(err){
+//           if (err) {
+//             console.log(err)
+//           } else {
+//             console.log("Whatever")
+//             callback(null, 'two')
+//           }
+//         })
+//       },
+//       function (callback) {
+//         console.log('ADD ROW')
+//         sheet.addRow({
+//           "name" : req.body.name,
+//           "split1": req.body.split1,
+//           "split2": req.body.split2,
+//           "split3": req.body.split3,
+//           "weight": req.body.weight
+//         }, function(err){
+//           if (err){
+//             console.log(err)
+//           } else {
+//             callback(null, 'three')
+//           }
+//         })
+//       },
+//     function(callback){
+//       console.log("In Get Cells")
+//       sheet.getRows({offset: 1,
+//       limit: 20,
+//       orderby: 'weight'}, function(err, rows){
+//         if (err) {
+//           console.log(err)
+//         } else {
+//           console.log(rows)
+//           res.json(rows);
+//
+//         }
+//       })
+//     }
+//   ])
+// });
 
 router.post('/createWorkout', function(req, res){
   var workoutTitle = req.body.title;
@@ -21,7 +208,7 @@ router.post('/createWorkout', function(req, res){
   var value1 = req.body.value1;
   var value2 = req.body.value2;
 
-  var Workout = new Workout({
+  var newWorkout = new Workout({
     workoutName: workoutTitle,
     athleteName: athleteName,
     workoutMetrics: [
@@ -36,15 +223,15 @@ router.post('/createWorkout', function(req, res){
     ]
   })
 
-  Workout.save(function(err, activityNew){
+  newWorkout.save(function(err, workoutNew){
     if (err) {
       console.log('error has occur: ',  err)
     } else {
       console.log('Nice, you created a file')
-      console.log(Workout);
+      console.log(workoutNew);
     }
-  }
+  })
 
-  res.json(Workout.metricsObject)
+  res.json(newWorkout.metricsObject)
 });
 module.exports = router;
