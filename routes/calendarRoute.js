32d20976e98cf42c65e7c4da6186ba9316bcb2e5
Julian Mazaira <mazairaj94@gmail.com>
2017-05-04 19:14:09 -0700

    heroku

--- routes/calendarRoute.js
+++ routes/calendarRoute.js
@@ -64,6 +64,7 @@ router.post('/getMonth', function(req, res){
   })
 })
 router.post('/findTeamWorkout', function(req, res) {
+  console.log(req.body.date)
   TeamWorkout.findOne({ "date": req.body.date})
   .populate('workouts')
   .exec(function(err, teamWorkout){
